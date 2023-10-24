29943ad4e35937cd617f0820f92c71c266e5b86f
silla <ibsilla2@gmail.com>
2023-10-25 03:21:51 +0400

    sss sdssss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import { useState } from 'react';
+import ReactPlayer from 'react-player';
+
+
+
+
+
+
 
 function App() {
+
+  const temp= {name: "", date: ""};
+  const [id, setId]=useState(1);
+  const [tasks, setTasks]= useState([]);
+
+  function changeName(event)
+  {
+    temp.name=event.target.value;
+  }
+
+  function changeDate(event)
+  {
+    temp.date=event.target.value.split("-").reverse().filter(d=>d.length<4).join("/");
+  }
+
+  function deleteHandler(event)
+  {
+    
+    const res= tasks.filter(data=>data.name!=event.target.innerHTML);
+    setTasks(res);
+  }
+ 
+  function submitHandler(event)
+  {
+    if(temp.name && temp.date)
+    {
+      const info =[...tasks, { name: temp.name, date: temp.date, id}];
+      setTasks(info);
+      temp.name="";
+      temp.date="";
+      setId(id+1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App bg-cover bg-[url('https://images.unsplash.com/photo-1590579491624-f98f36d4c763?auto=format&fit=crop&q=80&w=1443&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+      <header className="App-header rounded-b-lg mx-4 ">
+            <div className='font-bold text-white '>
+              <p>Welcome To My TODO List!</p>
+              <p><em className='font-normal italic '>@SillogicSolutions</em></p>
+
+            </div>
       </header>
+      <body className="App-body ">
+          <div className='w-2/3 my-10 border-4 p-6 rounded-t-lg bg-[#EDDFEA]'>
+            <div className='grid grid-cols-3'>
+                <label className='font-semibold'> Task Name 
+                  <input className="bg-gray-rounded-sm bg-red-300 h-10 " type="text" required onChange={changeName}/>
+                </label>
+                <label className='font-semibold'> Due Date
+                  <input className="rounded-sm bg-red-300 h-10 " type= "date" required onChange={changeDate}/>
+                </label>
+                <button onClick={submitHandler} className='rounded-sm bg-red-300 h-10 mt-8' > Save Task </button>
+            </div>
+            <br />
+            <div>
+              {tasks.map(function(d, index){
+                    return (
+                    <div key= { d.id}  className='grid grid-cols-2 border-b-2 border-dashed border-cyan-800 my-5 hover:bg-gray-400 hover:rounded-md' onClick={deleteHandler}>
+                      <p className='flex' title='Click to delete' >{d.name}</p>
+                      <p className='flex flex-row-reverse' >{d.date}</p>
+                    </div>
+                    ) })}
+            </div>
+          </div>
+      </body>
     </div>
   );
 }
